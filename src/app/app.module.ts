748054e13920844382982b74e638b2a082f6b993
Roman-code-web <selenerg.275@gmail.com>
2023-01-23 09:44:58 -0500

    firebaseconfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//-------
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+
+import { Environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+
+var config={ 
+        apiKey: Environment.firebase.apiKey,
+        authDomain: Environment.firebase.authDomain,
+        projectId: Environment.firebase.projectId,
+        storageBucket: Environment.firebase.storageBucket,
+        messagingSenderId: Environment.firebase.messagingSenderId,
+        appId: Environment.firebase.appId
+ }
 
 @NgModule({
   declarations: [
@@ -12,7 +26,9 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(()=>initializeApp(Environment.firebase)),
+    provideAuth(()=>getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const Environment = {
     firebase:{
-    apiKey: "AIzaSyAf4lOoG7mbBlDgJ6Qx2yqvsdyiqvJpMgI",
-    authDomain: "angular-2023-b9388.firebaseapp.com",
-    projectId: "angular-2023-b9388",
-    storageBucket: "angular-2023-b9388.appspot.com",
-    messagingSenderId: "982830657187",
-    appId: "1:982830657187:web:68a0028576e0d7aa7d1916"
-}
+        apiKey: "AIzaSyAf4lOoG7mbBlDgJ6Qx2yqvsdyiqvJpMgI",
+        authDomain: "angular-2023-b9388.firebaseapp.com",
+        projectId: "angular-2023-b9388",
+        storageBucket: "angular-2023-b9388.appspot.com",
+        messagingSenderId: "982830657187",
+        appId: "1:982830657187:web:68a0028576e0d7aa7d1916"
+    }
 }
